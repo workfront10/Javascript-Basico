890025f207024df22affe7ce9e5cd4bf50adda78
hideveloper20 <electronicmade9064@outlook.com>
2020-10-15 20:04:09 -0500

    Mejoras en las promesas

--- Async-CallBacks-Promise-Ajax-AsyncAwait/Promise.js
+++ Async-CallBacks-Promise-Ajax-AsyncAwait/Promise.js
@@ -26,88 +26,103 @@ miPromesa2.then(resp=>{
 })*/
 
 //Uso callback - De callback a promesas
-console.log("------------------------------")
+console.log("------------------------------");
 
-const getUsuariosIds = new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve([101,102,103,104])
-	},1000)
-})
+const getUsuariosIds = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve([101, 102, 103, 104]);
+  }, 1000);
+});
 
-const getUsuario = userId =>{
-	return new Promise((resolve,reject)=>{
-		setTimeout(id=>{
-			const usuario = {
-				email:'ejemplo@hotmail.com',
-				name:'Ejemplo'
-			}
+const getUsuario = (userId) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (id) => {
+        const usuario = {
+          email: "ejemplo@hotmail.com",
+          name: "Ejemplo",
+        };
 
-			resolve(`${id} - ${usuario.email} - ${usuario.name}`)
+        resolve(`${id} - ${usuario.email} - ${usuario.name}`);
+      },
+      1500,
+      userId
+    );
+  });
+};
 
-		},1500,userId)
-	})
-}
+const getPermisos = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (id) => {
+        const permisos = ["admin", "creador"];
+        resolve(permisos);
+      },
+      1500,
+      id
+    );
+  });
+};
 
-const getPermisos = id=>{
-	return new Promise((resolve,reject)=>{
-		setTimeout(id=>{
-			const permisos = ['admin','creador']
-			resolve(permisos)
-		},1500,id)
-	})
-}
+getUsuariosIds
+  .then((ids) => {
+    console.log(ids);
+    return getUsuario(ids[2]);
+  })
+  .then((usuario) => {
+    console.log(usuario);
+    return getPermisos(usuario.id);
+  })
+  .then((permisos) => {
+    console.log(permisos);
+  })
+  .catch(() => {
+    console.log("Error");
+  });
 
-getUsuariosIds.then(ids=>{
-	console.log(ids)
-	return getUsuario(ids[2])
-
-}).then(usuario=>{
-	console.log(usuario)
-	return getPermisos(usuario.id)
-}).then(permisos=>{
-	console.log(permisos)
-})
-.catch(()=>{
-	console.log("Error")
-})
+getUsuario(8)
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
 
 //----------------------
 
-const getPersonas = new Promise((resolve,reject)=>{
-	const arrObj = [{
-		name:"Carl",
-		edad:14
-	},
-	{
-		name:"Mark",
-		edad:25
-	}
-	]
+const getPersonas = new Promise((resolve, reject) => {
+  const arrObj = [
+    {
+      name: "Carl",
+      edad: 14,
+    },
+    {
+      name: "Mark",
+      edad: 25,
+    },
+  ];
 
-	setTimeout(()=>{
-		resolve(arrObj)
-	},1500)
+  setTimeout(() => {
+    resolve(arrObj);
+  }, 1500);
+});
 
-})
+const getPersonaid = (obj) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (user) => {
+        if (user) {
+          resolve(`${user.name} - ${user.edad}`);
+        } else {
+          reject("Usuario no encontrado");
+        }
+      },
+      1500,
+      obj
+    );
+  });
+};
 
-
-const getPersonaid = obj=>{
-	return new Promise((resolve,reject)=>{
-		setTimeout(user=>{
-			if(user){
-			resolve(`${user.name} - ${user.edad}`)
-			}else{
-				reject("Usuario no encontrado")
-			}	
-		},1500,obj)
-	})
-}
-
-
-getPersonas.then(personas=>{
-	console.log(personas)
-	return getPersonaid(personas[1])
-}).then(persona=>console.log(persona))
-.catch(error=>console.log(error))
-
-
+getPersonas
+  .then((personas) => {
+    console.log(personas);
+    return getPersonaid(personas[1]);
+  })
+  .then((persona) => console.log(persona))
+  .catch((error) => console.log(error));
